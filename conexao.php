9bb4087d786b75194bdf5c3c5ac2209ee8a6d591
gracevthlia <114101163+gracevthlia@users.noreply.github.com>
2022-11-07 16:35:06 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,19 @@
 <?php
-
-
-
-
-
-
-?>
+   
+   $servidor="localhost";
+   $usuario="root";
+   $senha="";
+   $dbName="cadastroelogin";
+   
+   $conexao=mysqli_connect($servidor, $usuario, $senha, $dbName);
+   if (!$conexao)
+   {
+      die ("Houve um erro: ".mysqli_connect_error());
+   }
+   else
+   {
+      echo "conexao efetuada com sucesso";
+   }
+   
+   ?>
+   
