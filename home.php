8c6b8ad920803ffb58c1d734b252a6102a689db8
gracevthlia <114101163+gracevthlia@users.noreply.github.com>
2022-12-12 17:36:12 -0300

    Add files via upload

--- home.php
+++ home.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home page</title>
     <link rel="stylesheet" href="style3.css">
+    <link rel="shortcut icon" href="home.png">
 </head>
 <body>
-    <h1> Realize seu cadastro ou login ;)</h1>
+    <h1>  Don't have an account? Register here or Sign in ;)</h1>
+    <img src="Online world-bro.svg" class="main-login-image" alt="index">
     <div class="box">
-    <a href ="pagina1.php">Login</a> 
-    <a href ="pagina2.php">Cadastro</a> 
+    <a href ="pagina1.php">Sign in</a> 
+    <a href ="pagina2.php">Register</a> 
 </div>
-<div class="center-login">
-<img src="Virtual reality-pana.svg" class="center-login-image" alt="index">
-</div>
+
 </body>
 </html>
